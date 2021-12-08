c349bb200f6a54a2e32115f0b8cd4550e8c5bc0a
Daniel DeGuire <danjdeguire@gmail.com>
2021-12-07 23:20:12 -0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App  text-center">
-    {/* Delete the two lines below */}
-    <h1 className="text-4xl font-bold pt-20">This is a starter template for you!</h1>
-    <h1 className="text-2xl font-bold pt-10">All the best with your React + Tailwind project! 😃</h1>
-    <a href="https://github.com/tanmayhinge/react-tailwind-template" target="_blank" rel="noreferrer" className="text-blue-500 underline">Read Documentation for this Template</a>
+      {/* Delete the two lines below */}
+      <h1 className="text-4xl font-bold pt-20">Pet Finder using React!</h1>
+      <h1 className="text-2xl font-bold pt-10">
+        Learning how to use React JS - Coming Soon!😃
+      </h1>
+      <a
+        href="https://github.com/Daniel-DeGuire"
+        target="_blank"
+        rel="noreferrer"
+        className="text-blue-500 underline"
+      >
+        Return to GitHub
+      </a>
     </div>
   );
 }
